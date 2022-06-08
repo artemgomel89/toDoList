34548ebd2b584a6328eaeb63760b358e21fca2da
Artsem Samonau <artemgomel89@gmail.com>
2022-06-08 16:03:50 +0300

    feat: add task form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.scss';
+import TaskForm from './components/TaskForm';
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <TaskForm />
         </div>
     );
 }
